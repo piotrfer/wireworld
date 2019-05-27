61b7e422cac71e9d769ef2dc70bb66d3eabde07e
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-05-27 21:12:16 +0200

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,3 @@
-package com.company;
-
 public class Matrix {
     private Cell[][] board;
     private int colNum;
@@ -17,54 +15,69 @@ public class Matrix {
 
     public int simulateGeneration() {
         int headNeigh = 0;
-        Matrix next = new Matrix(board, colNum, rowNum);
+        Cell[][] next = new Cell[rowNum][colNum];
+        for (int i = 0; i < rowNum; i++){
+            for (int j = 0; j <colNum; j++){
+                next[i][j]= new Cell(board[i][j].getStatus());}}
+
         for (int i = 0; i < rowNum; i++) {
             for (int j = 0; j < colNum; j++) {
                 if( board[i][j].getStatus() == Cell.EMPTY)
-                        next.getBoardElement(i,j).setStatus(Cell.EMPTY);
+                        next[i][j].setStatus(Cell.EMPTY);
+
                 else if( board[i][j].getStatus() == Cell.HEAD)
-                        next.getBoardElement(i, j).setStatus(Cell.TAIL);
+                    next[i][j].setStatus(Cell.TAIL);
+
                 else if( board[i][j].getStatus() == Cell.TAIL)
-                        next.getBoardElement(i, j).setStatus(Cell.COND);
+                    next[i][j].setStatus(Cell.COND);
+
                  else if( board[i][j].getStatus() == Cell.COND){
                         headNeigh=0;
                         if (i - 1 >= 0)
                              if (board[i - 1][j].getStatus() == Cell.HEAD) //top neighbour cell
-                            headNeigh++;
+                                headNeigh++;
+
                         if ((j + 1 < colNum) && (i - 1 >= 0))
                             if (board[i - 1][j + 1].getStatus() == Cell.HEAD) //right top neighbour cell
-                            headNeigh++;
+                                headNeigh++;
+
                         if (j + 1 < colNum)
                             if (board[i][j + 1].getStatus() == Cell.HEAD) //right neighbour cell
                                 headNeigh++;
+
                         if ((j + 1< colNum) && (i + 1 < rowNum))
                             if (board[i + 1][j + 1].getStatus() == Cell.HEAD) //right bottom neighbour cell
                                 headNeigh++;
+
                         if (i + 1 < rowNum)
                             if (board[i + 1][j].getStatus() == Cell.HEAD) //bottom neighbour cell
-                            headNeigh++;
+                                headNeigh++;
+
                         if ((j - 1 >= 0) && (i + 1 < rowNum))
                             if (board[i + 1][j - 1].getStatus() == Cell.HEAD) //left bottom neighbour cell
-                            headNeigh++;
+                                headNeigh++;
+
                         if (j - 1 >= 0)
                             if (board[i][j - 1].getStatus() == Cell.HEAD) //left neighbour cell
                                 headNeigh++;
+
                         if ((j - 1 >= 0) && (i - 1 >= 0))
                             if (board[i - 1][j - 1].getStatus() == Cell.HEAD) //left top neighbour cell
                                 headNeigh++;
 
                         if (headNeigh == 1 || headNeigh == 2 )
-                            next.getBoardElement(i, j).setStatus(Cell.HEAD);
-                        System.out.println(headNeigh);
+                            next[i][j].setStatus(Cell.HEAD);
                 }
 
             }
 
         }
-        this.board = next.board;
+        this.board = next;
         return 0;
     }
 }
+
+
 
 
 
