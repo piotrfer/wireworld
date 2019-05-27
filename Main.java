2a85bfbdef72961bcb7bd75577a217024314cc26
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-05-27 21:10:38 +0200

    umożliwia sprawdzenie działania symulacji

--- Main.java
+++ Main.java
@@ -1,24 +1,24 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-        Cell[][] board = new Cell[4][4];
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 4; j++){
+        int row=5;
+        int col=6;
+        Cell[][] board = new Cell[row][col];
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j <col; j++){
                 board[i][j]= new Cell((int)(Math.random()*4));}}
 
-       for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
+       for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
                 System.out.print(board[i][j].getStatus() + " ");
             System.out.println(" ");}
 
-        Matrix test = new Matrix(board, 4, 4);
+        Matrix test = new Matrix(board, col, row);
         test.simulateGeneration();
         System.out.println(" ");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-                System.out.print(board[i][j].getStatus() + " ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                System.out.print(test.getBoardElement(i,j).getStatus() + " ");
             System.out.println(" ");
         }
     }
